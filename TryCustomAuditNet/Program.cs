848b0f4d82adf1bfd19c1ef567ef011d6b20e475
asinta <asinta2013@hotmail.com>
2020-11-12 21:04:57 +0800

    finish

--- TryCustomAuditNet/Program.cs
+++ TryCustomAuditNet/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Audit.Core;
+using Newtonsoft.Json;
 
 namespace TryCustomAuditNet
 {
@@ -6,7 +8,30 @@ namespace TryCustomAuditNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConfigureAudit();
+            
+            var order = new Order(Guid.NewGuid(), "Jone Doe", 100, DateTime.UtcNow);
+            using (var scope = AuditScope.Create("Order::Update", () => order))
+            {
+                order.UpdateOrderAmount(200);
+                
+                // optional
+                scope.Comment("this is a test for update order.");
+            }
+        }
+        
+        private static void ConfigureAudit()
+        {
+            Audit.Core.Configuration.Setup()
+                .UseCustomProvider(new CustomFileDataProvider(config => config
+                    .DirectoryBuilder(_ => "./")
+                    .FilenameBuilder(auditEvent => $"{auditEvent.EventType}_{DateTime.Now.Ticks}.json")
+                    .JsonSettings(new JsonSerializerSettings
+                    {
+                        Formatting = Formatting.Indented,
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        NullValueHandling = NullValueHandling.Include
+                    })));
         }
     }
 }
